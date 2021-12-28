94fb28d745812e34a868775571ad7a4f974a47a9
Axel Halifa <axel.halifa0@gmail.com>
2021-12-28 16:33:14 +0100

    Changement de component "Class" en "Fonction"

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,48 +2,45 @@ import React from "react";
 import * as Styles from "./styles";
 import { Link } from "react-router-dom";
 
-export class Home extends React.Component {
-  parentHomeStyle = {
+export function Home() {
+  const [champions, getChampions] = React.useState(null);
+
+  const parentHomeStyle = {
     display: "flex",
     justifyContent: "center",
   };
 
-  homeStyle = {
+  const homeStyle = {
     flexWrap: "wrap",
     justifyContent: "space-around",
     width: "50%",
     ...Styles.row,
   };
 
-  constructor() {
-    super();
-    this.state = { champions: [] };
-  }
-
-  componentDidMount() {
+  React.useEffect(() => {
     fetch("/api/champions/list")
       .then((res) => res.json())
-      .then((champions) => this.setState({ champions: champions }));
-  }
+      .then((champions) => getChampions(champions));
+  }, []);
 
-  render() {
-    let champions = this.state.champions;
-    let ids = champions.map((champion) => {
-      return (
-        <div>
-          <Link to={"champion/" + champion.id}>
-            <img src={champion.championImg} />
-          </Link>
-          <p>{champion.name}</p>
-        </div>
-      );
-    });
 
+  if (champions == null) {
+    return <div>Page en cours de chargement</div>;
+  } else {
     return (
       <div>
         <h1 style={Styles.container}>Personnages</h1>
-        <div style={this.parentHomeStyle}>
-          <div style={this.homeStyle}>{ids}</div>
+        <div style={parentHomeStyle}>
+          <div style={homeStyle}>
+            {champions.map((champion) => (
+              <div>
+                <Link to={"champion/" + champion.id}>
+                  <img src={champion.championImg} />
+                </Link>
+                <p>{champion.name} </p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     );
